27e2c2d7a5252169c83b908300abf3acd12adc65
Jongmin Jack Kim <jmjkim00@icloud.com>
2022-07-31 23:55:28 -0700

    Completed assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,22 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
   # add routes
+  get '/bakeries' do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
 
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    BakedGood.order(price: :desc).to_json
+  end
+
+  get '/baked_goods/most_expensive' do 
+    BakedGood.order(price: :desc).first.to_json
+  end
 end
